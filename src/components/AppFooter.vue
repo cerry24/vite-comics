cbf03bde4eff9fd209b40bf22b0dcb6dc46f505c
cerry24 <samuelecerretti@gmail.com>
2023-01-09 17:53:03 +0100

    implement upper footer macrostructure

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,10 +4,115 @@ export default {
 }
 </script>
 <template>
-    <div>
+    <section id="upper-footer">
+        <div class="footer-container">
+            <div class="footer-nav">
+                <div class="queueing-lists">
+                    <ul>
+                        <li>
+                            <h4>dc comics</h4>
+                        </li>
+                        <li>oggetto</li>
+                        <li>oggetto</li>
+                        <li>oggetto</li>
+                        <li>oggetto</li>
+                        <li>oggetto</li>
+                        <li>oggetto</li>
+                        <li>oggetto</li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <h4>shop</h4>
+                        </li>
+                        <li>oggetto</li>
+                        <li>oggetto</li>
+                    </ul>
+                </div>
 
-    </div>
+                <ul>
+                    <li>
+                        <h4>dc</h4>
+                    </li>
+                    <li>oggetto</li>
+                    <li>oggetto</li>
+                    <li>oggetto</li>
+                    <li>oggetto</li>
+                    <li>oggetto</li>
+                    <li>oggetto</li>
+                    <li>oggetto</li>
+                    <li>oggetto</li>
+                    <li>oggetto</li>
+                    <li>oggetto</li>
+                    <li>oggetto</li>
+                </ul>
+
+                <ul>
+                    <li>
+                        <h4>sites</h4>
+                    </li>
+                    <li>oggetto</li>
+                    <li>oggetto</li>
+                    <li>oggetto</li>
+                    <li>oggetto</li>
+                    <li>oggetto</li>
+                </ul>
+            </div>
+
+            <div class="logo-big">
+                <img src="../assets/img/dc-logo-bg.png" alt="Big logo Dc">
+            </div>
+        </div>
+    </section>
 </template>
-<style lang="scss">
 
+<style lang="scss">
+#upper-footer {
+    height: 400px;
+    background-image: url(../assets/img/footer-bg.jpg);
+    background-size: cover;
+    justify-content: space-between;
+    overflow: hidden;
+}
+
+.footer-container {
+    width: 70%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+}
+
+.footer-nav {
+    display: flex;
+    margin: 3rem 0;
+
+    ul {
+        list-style-type: none;
+        margin-right: 2rem;
+        margin-bottom: 1rem;
+
+        li {
+            font-size: 0.9rem;
+            line-height: 1.4rem;
+            color: gray;
+        }
+    }
+}
+
+
+h4 {
+    text-transform: uppercase;
+    margin-bottom: 0.8rem;
+    font-size: 1rem;
+    color: white;
+}
+
+.logo-big {
+    position: relative;
+
+    img {
+        position: absolute;
+        right: 0;
+        top: -100px;
+    }
+}
 </style>
