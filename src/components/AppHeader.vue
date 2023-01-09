24898c2dab8b2b9d2e3697f4e6a2135c94dca70a
cerry24 <samuelecerretti@gmail.com>
2023-01-09 17:08:09 +0100

    implement AppHeader content

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,10 +4,59 @@ export default {
 }
 </script>
 <template>
-    <div>
-
+    <div class="header-container">
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="Dc small logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">oggetto</a></li>
+                <li><a href="#">oggetto</a></li>
+                <li><a href="#">oggetto</a></li>
+                <li><a href="#">oggetto</a></li>
+                <li><a href="#">oggetto</a></li>
+                <li><a href="#">oggetto</a></li>
+                <li><a href="#">oggetto</a></li>
+                <li><a href="#">oggetto</a></li>
+                <li><a href="#">oggetto</a></li>
+                <li><a href="#">oggetto</a></li>
+            </ul>
+        </nav>
     </div>
 </template>
-<style lang="scss">
 
+<style lang="scss">
+.header-container {
+    width: 70%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.logo {
+    padding: 0.8rem 0;
+
+    img {
+        width: 75%;
+    }
+}
+
+nav ul {
+    list-style-type: none;
+
+    li {
+        display: inline-block;
+        text-transform: uppercase;
+        font-weight: 700;
+        font-size: 0.9rem;
+        color: black;
+        padding: 0 0.8rem;
+    }
+
+}
+
+a {
+    text-decoration: none;
+}
 </style>
