8d8d4082957a2bd1c8e129eed9e412675a469af8
cerry24 <samuelecerretti@gmail.com>
2023-01-09 17:24:36 +0100

    implement main macrostructure

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,10 +4,50 @@ export default {
 }
 </script>
 <template>
-    <div>
+    <section id="content">
+        <div class="content-container">
+            <h3>--> Content goes here</h3>
+        </div>
+    </section>
 
-    </div>
+    <section id="dc-universe">
+        <ul>
+            <li>oggetto</li>
+            <li>oggetto</li>
+            <li>oggetto</li>
+            <li>oggetto</li>
+            <li>oggetto</li>
+        </ul>
+    </section>
 </template>
-<style lang="scss">
 
+<style lang="scss">
+#content {
+    background-color: black;
+    padding: 3rem 0;
+
+    .content-container {
+        width: 70%;
+        color: white;
+        margin: 0 auto;
+    }
+}
+
+#dc-universe {
+    background-color: #0282f9;
+    padding: 4rem 0;
+
+    ul {
+        display: flex;
+        justify-content: space-between;
+        width: 70%;
+        list-style-type: none;
+        color: white;
+        margin: 0 auto;
+
+        li {
+            margin: 1rem;
+        }
+    }
+}
 </style>
